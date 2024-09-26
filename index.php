fb81b7ec6be49031ea9ab2e18b26c2e7b93efe24
fretka46 <fretka46.02@gmail.com>
2024-09-26 17:18:16 +0200

    Editor changes & simplification

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ require "functions.php";
     <meta charset="UTF-8">
     <title>Teacher List</title>
     <link rel="stylesheet" href="css/mainPage.css">
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.10/dist/full.min.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/daisyui@4.12.10/dist/full.min.css" type="text/css" />
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
@@ -39,18 +39,21 @@ require "functions.php";
                 } else {
                     foreach ($teachers as $teacher) {
                         $rowClass = $teacher['is_present'] ? 'bg-green-100 bg-opacity-20' : 'bg-red-100 bg-opacity-20';
-                        echo "<tr class='$rowClass'>";
-                        echo "<td class='w-1/12 text-center'>" . $teacher['id'] . "</td>";
-                        echo "<td class='w-2/12 flex items-center gap-3 justify-center'>
+
+                        echo /*html*/"
+                        <tr class='$rowClass'>
+                        <td class='w-1/12 text-center'>" . $teacher['id'] . "</td>
+                        <td class='w-2/12 flex items-center gap-3 justify-center'>
                                 <div class='avatar ml-6 " . ($teacher['is_present'] ? 'online' : 'offline') . "'>
                                     <div class='w-24 rounded-full'>
                                         <img src='api/profiles/" . $teacher['id'] . ".jpg' alt='Profile Picture' />
                                     </div>
                                 </div>
-                              </td>";
-                        echo "<td class='w-4/12 text-center'>" . $teacher['name'] . "</td>";
-                        echo "<td class='w-2/12 text-center'>" . ($teacher['is_present'] ? "Present" : "Absent") . "</td>";
-                        echo "</tr>";
+                              </td>
+                        <td class='w-4/12 text-center'>" . $teacher['name'] . "</td>
+                        <td class='w-2/12 text-center'>" . ($teacher['is_present'] ? "Present" : "Absent") . "</td>
+                        </tr>
+                        ";
                     }
                 }
                 ?>
